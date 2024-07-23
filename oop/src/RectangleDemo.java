80318ac5d2b76b4ba9435748653a6e8a5894c281
Hani <haniali1204@gmail.com>
2024-07-23 15:13:31 +0100

    Remove submodule java-revision and add House, Rectangle, and RectangleDemo classes

---
+++ oop/src/RectangleDemo.java
@@ -0,0 +1,25 @@
+package oop.src;
+
+public class RectangleDemo {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = new Rectangle(5, 10);
+
+        System.out.println(Rectangle.getNumRectangles());
+
+        Rectangle r3;
+
+        System.out.println("After r3 declared " + Rectangle.getNumRectangles());
+
+        r3 = new Rectangle(2.5, 4);
+
+        System.out.println("after r3 instantiantied " + Rectangle.getNumRectangles());
+
+        System.out.println(r1.area());
+        System.out.println(r2.area());
+        System.out.println(r3.area());
+        
+        System.out.println(r1.perimeter());
+    }// end main
+    
+}
